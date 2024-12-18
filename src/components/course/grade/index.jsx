74c7cfe7eacb7c3a558b203e7faa4292539a2f2f
NaCl <120160850+NaClCode@users.noreply.github.com>
2024-12-19 00:18:44 +0800

    🐞 fix(grade): 可以正确显示成绩

--- src/components/course/grade/index.jsx
+++ src/components/course/grade/index.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { Input, Button, message, Select } from 'antd';
+import { message } from 'antd';
 import { ProCard, ProTable } from '@ant-design/pro-components';
 import { service } from '@/service';
-import { SearchOutlined, CloseCircleOutlined } from '@ant-design/icons';
+
 import './grade.scss';
 
 const courseTypeMap = {
@@ -13,8 +13,8 @@ const courseTypeMap = {
 };
 
 const columns = () => [
-  { title: '课程号', dataIndex: 'id', ellipsis: true, width: 100 },
-  { title: '课程名称', dataIndex: 'name', ellipsis: true },
+  { title: '课程号', dataIndex: 'course_id', ellipsis: true, width: 100 },
+  { title: '课程名称', dataIndex: 'course_name', ellipsis: true },
   { title: '专业', dataIndex: 'profession', ellipsis: true },
   { title: '学院', dataIndex: 'college', ellipsis: true },
   {
@@ -26,7 +26,7 @@ const columns = () => [
   },
   { title: '学分', dataIndex: 'credit', ellipsis: true, width: 100 },
   { title: '教师', dataIndex: 'teacher', ellipsis: true },
-  { title: '分数', dataIndex: 'score', ellipsis: true, width: 100 },
+  { title: '分数', dataIndex: 'grade', ellipsis: true, width: 100 },
 ];
 
 const CourseGrades = () => {
@@ -35,12 +35,19 @@ const CourseGrades = () => {
   const [page, setPage] = useState(1);
   const [pageSize, setPageSize] = useState(9);
   const [total, setTotal] = useState(0);
-  const [inputSearchParams, setInputSearchParams] = useState({ name: '', college: '', profession: '', type: '', teacher: '' });
+  const [inputSearchParams, setInputSearchParams] = useState({
+    name: '',
+    college: '',
+    profession: '',
+    type: '',
+    teacher: '',
+  });
 
+  // Fetch grades from the server
   const fetchGrades = async (currentPage = page, currentPageSize = pageSize, params = inputSearchParams) => {
     setLoading(true);
     try {
-      const res = await service.coursegrades.search(
+      const res = await service.course.grade(
         currentPage,
         currentPageSize,
         params.name,
@@ -63,22 +70,12 @@ const CourseGrades = () => {
 
   useEffect(() => {
     fetchGrades();
-  }, [page, pageSize]);
+  }, []);
 
   const handleTableChange = (pagination) => {
     setPage(pagination.current);
     setPageSize(pagination.pageSize);
-  };
-
-  const handleSearch = () => {
-    setPage(1);
-    fetchGrades(1, pageSize);
-  };
-
-  const handleClearSearch = () => {
-    setInputSearchParams({ name: '', college: '', profession: '', type: '', teacher: '' });
-    setPage(1);
-    fetchGrades(1, pageSize);
+    fetchGrades(pagination.current, pagination.pageSize);
   };
 
   return (
@@ -97,51 +94,6 @@ const CourseGrades = () => {
         options={{ density: false }}
         className="protable"
         onChange={handleTableChange}
-        toolBarRender={() => [
-          <Input
-            key="name"
-            placeholder="课程名称"
-            value={inputSearchParams.name}
-            onChange={(e) => setInputSearchParams({ ...inputSearchParams, name: e.target.value })}
-            className='search-input'
-          />,          
-          <Input
-            key="profession"
-            placeholder="专业"
-            value={inputSearchParams.profession}
-            onChange={(e) => setInputSearchParams({ ...inputSearchParams, profession: e.target.value })}
-            className='search-input'
-          />,
-          <Input
-            key="college"
-            placeholder="学院"
-            value={inputSearchParams.college}
-            onChange={(e) => setInputSearchParams({ ...inputSearchParams, college: e.target.value })}
-            className='search-input'
-          />,
-          <Input
-            key="teacher"
-            placeholder="教师"
-            value={inputSearchParams.teacher}
-            onChange={(e) => setInputSearchParams({ ...inputSearchParams, teacher: e.target.value })}
-            className='search-input'
-          />,
-          <Select
-            className="filter-select"
-            key="type"
-            placeholder="课程类型"
-            value={inputSearchParams.type}
-            onChange={(value) => setInputSearchParams({ ...inputSearchParams, type: value })}
-          >
-            <Option value="">全部</Option>
-            <Option value="B">必修</Option>
-            <Option value="X">选修</Option>
-            <Option value="G">公选</Option>
-            <Option value="S">实践</Option>
-          </Select>,
-          <Button className="search-button" onClick={handleSearch} type="default" icon={<SearchOutlined />} />,
-          <Button className="clear-button" onClick={handleClearSearch} type="default" icon={<CloseCircleOutlined />} />,
-        ]}
       />
     </ProCard>
   );
